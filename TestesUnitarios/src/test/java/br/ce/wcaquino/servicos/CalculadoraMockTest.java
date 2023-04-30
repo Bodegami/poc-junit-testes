9a5eb3cf1298d0d852b6ab3305c065f36768f4ae
Renato <rena.f.oliveira@gmail.com>
2023-04-29 22:10:27 -0300

    Spy

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,12 +1,54 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Spy
+	private EmailService emailServiceSpy;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void devoMostrarDiferencaEntreMockESpy() {
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(5);
+		//Mockito.when(calcSpy.somar(1, 2)).thenReturn(5);
+		Mockito.doReturn(5).when(calcSpy).somar(1, 2);
+		Mockito.doNothing().when(calcSpy).imprime();
+		
+		//Retorna 0 que é o valor default quando passado valor diferente da expectativa
+		System.out.println("Mock: " + calcMock.somar(1, 2));
+		
+		//Chama o metodo real quando passado valor diferente da expectativa
+		//Por conta dessa caracteristica, o Spy não funciona com interfaces, apenas classes concretas
+		System.out.println("Spy: " + calcSpy.somar(1, 2));
+		
+		System.out.println("Mock");
+		calcMock.imprime();
+		System.out.println("Spy");
+		calcSpy.imprime();
+	}
+	
+	
+	
+	
 	
 	@Test
 	public void teste() {
@@ -15,6 +57,7 @@ public class CalculadoraMockTest {
 		//e no passarmos um matcher, todos os outros parametros devem ser matchers.
 		//Caso seja preciso passar um parametro especifico e nos outros os marchers,
 		//podemos utilizar o "eq" passando o valor dentro das aspas.
+
 		Calculadora calc = Mockito.mock(Calculadora.class);
 		
 		ArgumentCaptor<Integer> argCapt = ArgumentCaptor.forClass(Integer.class);
